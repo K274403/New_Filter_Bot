5dd283bcdd482fa9ee904877d4733579409bf2f1
K274403 <158284550+K274403@users.noreply.github.com>
2025-03-02 06:21:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -271,7 +271,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """[@RKMOVIESZIP]<a href="https://t.me/Rkmovieszip">{filename}</a> {languages}
+    CAPTION = """[@RKMOVIESZIP]<a href="https://t.me/Rkmovieszip">{file_name}</a> {languages}
 
 <blockquote>✅ 𝘼𝙡𝙨𝙤 𝙟𝙤𝙞𝙣 #𝙍𝙠𝙢𝙤𝙫𝙞𝙚𝙯𝙞𝙥 – #𝙉𝙤𝟏 𝙏𝙚𝙡𝙚𝙜𝙧𝙖𝙢 𝙁𝙧𝙚𝙚 𝙈𝙤𝙫𝙞𝙚𝙨 𝘼𝙣𝙙 𝙒𝙚𝙗 𝙎𝙚𝙧𝙞𝙚𝙨 𝙀𝙣𝙩𝙚𝙧𝙩𝙖𝙞𝙣𝙢𝙚𝙣𝙩 𝘾𝙝𝙖𝙣𝙣𝙚𝙡. 
 
