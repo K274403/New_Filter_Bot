1b8f5c0a9a1e693b0f77dafe17e71520316ae7f4
Krishna Prajapati <158284550+K274403@users.noreply.github.com>
2025-06-05 08:03:10 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,8 +3,6 @@ class script(object):
 
 Hey {}, {} 👋
 
-💪🤖 I'm not just *any* bot... I’m the 🧠 **Sanskari AI** running on 100% devotion and 200% caffeine!
-
 ⚡️ 𝓘 𝓪𝓶 𝓽𝓱𝓮 𝓶𝓸𝓼𝓽 𝓪𝓭𝓿𝓪𝓷𝓬𝓮𝓭 𝓐𝓾𝓽𝓸 𝓕𝓲𝓵𝓽𝓮𝓻 𝓑𝓸𝓽 with:
 - 💎 Premium Vibes
 - 🕉️ Bhakti Mode ON
@@ -87,9 +85,9 @@ Nᴀᴍᴇ - {}
 
     CUDNT_FND = """<b>𝑆𝑝𝑒𝑙𝑙𝑖𝑛𝑔 𝑀𝑖𝑠𝑡𝑎𝑘𝑒 𝐵𝑟𝑜 ‼️\n𝐷𝑜𝑛'𝑡 𝑊𝑜𝑟𝑟𝑦 😊 𝐶ℎ𝑜𝑜𝑠𝑒 𝑇ℎ𝑒 𝐶𝑜𝑟𝑟𝑒𝑐𝑡 𝑂𝑛𝑒 𝐵𝑒𝑙𝑜𝑤 👇\n\n<blockquote>नीचे दिए गए विकल्पों में से movie के नाम की सही Spelling चुनें 👇</blockquote></b>"""
 
-    I_CUDNT = """<b>sᴏʀʀʏ ɴᴏ ꜰɪʟᴇs ᴡᴇʀᴇ ꜰᴏᴜɴᴅ ꜰᴏʀ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛ {} 😕
+    I_CUDNT = """<b>फ़ाइल को ढूंढा बहुत, पर वो मिली नहीं, सिस्टम भी कहता है – अब उसकी उम्मीद बची नहीं... {} 😕
 
-ᴄʜᴇᴄᴋ ʏᴏᴜʀ sᴘᴇʟʟɪɴɢ ɪɴ ɢᴏᴏɢʟᴇ ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ 😃
+📢 Kripya dobara try karein, ya system ki maa-behen mat karein. 😅
 
 📝 ᴍᴏᴠɪᴇ ʀᴇǫᴜᴇꜱᴛ ꜰᴏʀᴍᴀᴛ 👇
 
